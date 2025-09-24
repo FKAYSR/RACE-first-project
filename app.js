7a698dbd5a4f09aaf9830763c59fe2b3339e5f32
FKAYSR <kejserfreja@gmail.com>
2025-09-24 13:02:41 +0200

    java

---
+++ app.js
@@ -0,0 +1,19 @@
+
+document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "Hello JavaScript!";
+// alert("Hello Multimedia Designers");
+console.log("Hello Multimedia Designers");
+console.log("Hi Students!");
+console.log("This is your first JavaScript App 🎉");
+
+function sayHello() {}
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+
+document.querySelector("button").addEventListener("click", sayHello);
+
+
+
